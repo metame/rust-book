17e0bc9685cda44b1b6939fc14b587a54087b019
Michael Erwin <michaeljameserwin@gmail.com>
2017-07-08 00:52:43 -0400

    finish functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -27,6 +27,46 @@ fn main() {
     }
     println!("Add one to 5 to get: {}", add_one(5));
 
+    // There's a difference between an expression and a statement
+    // Expressions return a value and statements do not
+    // There are only two kinds of statements in rust: declaration statements and expression statements
+
+    // Declaration statements (e.g. variable declaration)
+    // js/ruby-like syntax of `let x = y = 5` aren't valid in Rust 
+    // let x = (let y = 5); // throws Expected Identifier, found keyword 'let'
+    // since a statement doesn't return anything the valid syntax below doesn't do what you'd think it would coming from js/ruby
+    let mut y = 5;
+    let x = y = 6;
+    assert!(x == ()); // an empty struct
+    assert!(y == 6);
+
+    // Expression statements (e.g. implied return )
+    // the `add_one` fn above uses this, it makes the `x+1` expression into a return statement by omitting the semi-colon
+
+    // Early returns can be done using `return`
+    fn foo2(x: i32) -> i32 {
+        return x;
+
+        // dead code
+        x + 1
+    }
+
+
+    // Function pointers, binding variables to fn's
+    let f: fn(i32) -> i32;
+    
+
+
+    // Diverging functions (functions that don't return)
+    // the type `!` is read 'diverges'
+    fn diverges() -> ! {
+        panic!("This function never returns");
+    }
+    diverges(); //thread 'main' panicked at 'This function never returns'
+
+
+
+
 }
 
 
